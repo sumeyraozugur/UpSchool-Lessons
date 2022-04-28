4c555e635f636ce5786e334783cc211988fb44c8
123Sumeyra <ozugursumeyra@gmail.com>
2022-04-29 00:30:32 +0300

    avatar dont show

--- 16Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
+++ 16Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sum.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var image:ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        image = findViewById(R.id.imageView)
+        val anim = AnimationUtils.loadAnimation(this, R.anim.my_anim)
+        image.startAnimation(anim)
     }
 }
