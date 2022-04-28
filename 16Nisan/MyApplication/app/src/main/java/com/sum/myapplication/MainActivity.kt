e58c9133429a529aae4f5b3381e8dc1099a1c861
123Sumeyra <ozugursumeyra@gmail.com>
2022-04-29 01:05:15 +0300

    first anim

--- 16Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
+++ 16Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
@@ -3,17 +3,24 @@ package com.sum.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var image:ImageView
+    private lateinit var button: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         image = findViewById(R.id.imageView)
+        button =findViewById(R.id.button)
         val anim = AnimationUtils.loadAnimation(this, R.anim.my_anim)
-        image.startAnimation(anim)
+        button.setOnClickListener {
+            image.startAnimation(anim)
+
+        }
+
     }
 }
