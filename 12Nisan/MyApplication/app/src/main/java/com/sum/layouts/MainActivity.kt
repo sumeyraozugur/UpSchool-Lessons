2f1cb8a611a3bea1aaa7f2a21dc4d098b7cac0ab
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-03 14:59:02 +0300

    Data Binding

--- 12Nisan/MyApplication/app/src/main/java/com/sum/layouts/MainActivity.kt
+++ 12Nisan/MyApplication/app/src/main/java/com/sum/layouts/MainActivity.kt
@@ -2,10 +2,34 @@ package com.sum.layouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.sum.layouts.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =DataBindingUtil.setContentView(this@MainActivity,R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        init()
+    }
+    private fun init(){
+        binding.myButton.text ="asakjskaskask"
+        binding.myButton.setOnClickListener(this)
+        binding.myButton2.setOnClickListener(this)
+        binding.myText
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            binding.myButton.id -> {
+                //Click Listener
+            }
+            binding.myButton2.id ->{}
+        }
     }
 }
