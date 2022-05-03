3c850e75f2e5a6c6bb819cedb306e7098ffe2a7a
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-04 01:35:00 +0300

    dark mode

--- 14Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
+++ 14Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
@@ -2,10 +2,13 @@ package com.sum.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
