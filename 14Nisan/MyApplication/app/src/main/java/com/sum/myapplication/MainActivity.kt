1d34a7b0915594fde3cb8da5e23559f003f48b8d
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-04 02:51:51 +0300

    dark mode with switchmaterial

--- 14Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
+++ 14Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
@@ -2,13 +2,32 @@ package com.sum.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener {
+    private lateinit var switchMaterial:SwitchMaterial
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        switchMaterial = findViewById(R.id.mySwitch)
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        switchMaterial.setOnCheckedChangeListener(this)
+
+
     }
+
+    override fun onCheckedChanged(p0: CompoundButton?, isChecked: Boolean) {
+      if(isChecked){
+          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+      }else{
+          AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+      }
+    }
+
 }
+
