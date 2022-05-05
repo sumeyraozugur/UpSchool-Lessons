6784a24ff0e74e8594735b26accb64010a576aa6
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-05 16:04:47 +0300

    navigation Draver V1

---
+++ 26Nisan/NavigationDrawer/app/src/main/java/com/sum/navigationdrawer/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.sum.navigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val navigationView: NavigationView = findViewById(R.id.nav_graph)
+        navigationView.setupWithNavController(navController)
+
+    }
+}
