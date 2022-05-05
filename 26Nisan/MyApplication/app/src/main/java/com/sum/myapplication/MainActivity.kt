647ce2058f1e53368c3e06387d98ed936807a848
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-05 14:19:10 +0300

    somthing fixed about menu and bottom navigation done

--- 26Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
+++ 26Nisan/MyApplication/app/src/main/java/com/sum/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.sum.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigation.setupWithNavController(navController)
+
+
     }
 }
