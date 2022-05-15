ba399c4f954284313711b327bb74b54c3062756d
123Sumeyra <ozugursumeyra@gmail.com>
2022-05-15 15:05:20 +0300

    with databinding

--- Lesson14/MVVM/app/src/main/java/com/sum/mvvm/MainActivity.kt
+++ Lesson14/MVVM/app/src/main/java/com/sum/mvvm/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.sum.mvvm
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.sum.mvvm.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
-        val viewModelFactory = FinalScoreViewModelFactory(10)
-        val viewModel = ViewModelProvider(this, viewModelFactory).get(FinalScoreViewModel::class.java)
+        val viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        binding.gameViewModel = viewModel
+        binding.setLifecycleOwner(this)
 
+
+        viewModel.eventGameFinish.observe(this, Observer{ isGameFinished ->
+            if(isGameFinished){
+                Toast.makeText(this,"Game Finished",Toast.LENGTH_LONG).show()
+            }
+
+        })
 
 
 
